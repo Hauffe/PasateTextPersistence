19d7346aff5e3278532eb6e898960e0f3c2d297d
Alexandre Hauffe <alex_hauffe@hotmail.com>
2020-09-26 19:21:39 -0300

    Initial Commit

---
+++ PasteTextPersistence/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PasteTextPersistence
+//
+//  Created by Alex on 26/09/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
