c8f92164bce9163a16060f0d1f166e95df961f47
Alexandre Hauffe <alex_hauffe@hotmail.com>
2020-09-26 19:52:49 -0300

    paste and delete info app

--- PasteTextPersistence/ViewController.swift
+++ PasteTextPersistence/ViewController.swift
@@ -8,12 +8,40 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let DATA_KEY = "data_key"
+    
+    var pastedStriongs: [String] = []
 
+    @IBOutlet weak var textView: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        if let loadedStrings = UserDefaults.standard.stringArray(forKey: DATA_KEY){
+            pastedStriongs.append(contentsOf: loadedStrings)
+        }
+        showText()
     }
-
-
+    
+    func addText(){
+        guard let text = UIPasteboard.general.string, !pastedStriongs.contains(text) else{
+            return
+        }
+        pastedStriongs.append(text)
+        UserDefaults.standard.set(pastedStriongs, forKey: DATA_KEY)
+        showText()
+    }
+    
+    func showText(){
+        textView.text = ""
+        for str in pastedStriongs{
+            textView.text.append("\(str)\n\n")
+        }
+    }
+    
+    @IBAction func trashPressed(_ sender: Any) {
+        textView.text = ""
+        pastedStriongs.removeAll()
+        UserDefaults.standard.removeObject(forKey: DATA_KEY)
+    }
 }
 
